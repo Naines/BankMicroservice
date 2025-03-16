57e43eb16c90c270d4241eed5a7c00203acd8222
Nainesh Goel <naineshgoel96@gmail.com>
2025-03-17 04:02:50 +0530

    refactor: ckt breaker initial

---
+++ section10/cards/src/main/java/com/nainesh/cards/CardsApplication.java
@@ -0,0 +1,19 @@
+package com.nainesh.cards;
+
+import com.nainesh.cards.dto.CardsContactInfoDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableConfigurationProperties( value = {CardsContactInfoDto.class})
+@EnableJpaAuditing(auditorAwareRef = "AuditAwareImpl")
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
